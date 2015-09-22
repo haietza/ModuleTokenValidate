f29fb7294c7def521c0feabb6cd162337bf62682
Michelle <Michelle@10.0.2.15>
2015-09-22 10:15:00 -0400

    Tested to work from remote web service on RTMP.

--- src/edu/appstate/lts/ModuleTokenValidate.java
+++ src/edu/appstate/lts/ModuleTokenValidate.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -36,21 +37,24 @@ public class ModuleTokenValidate extends ModuleBase implements IMediaStreamNameA
 		super();
 	}
 	
-	private String validateToken(String token, String Url, String IpAddress) {
+	private String validateToken(String token, String url, String ipAddress) {
 		String hashValue = null;
 		String wsApplication;
 		String wsUrl = null;
 		String wsIpAddress = null;
 		
-		getLogger().info("VT Token: " + token);
-		getLogger().info("VT URL: " + Url);
-		getLogger().info("VT IP: " + IpAddress);
+		getLogger().info("Arguments passed to validateToken from resolvePlayAlias: Token = " + token + ", URL = " + url + ", IP = " + ipAddress);
 		
 		// Get token info from web service
 		try {
+			URL ticket = new URL("http://lts6.lts.appstate.edu/mensch-tickets/ticket/" + token);
+			HttpURLConnection connection = (HttpURLConnection) ticket.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Accept", "application/xml");
+			
 			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder builder = factory.newDocumentBuilder();
-			Document doc = builder.parse(new URL("http://lts6.lts.appstate.edu/mensch-tickets/ticket/" + token).openConnection().getInputStream());
+			Document doc = builder.parse(connection.getInputStream());
 			doc.getDocumentElement().normalize();
 			NodeList nList = doc.getElementsByTagName("Ticket");
 			for (int i = 0; i < nList.getLength(); i++) {
@@ -63,9 +67,7 @@ public class ModuleTokenValidate extends ModuleBase implements IMediaStreamNameA
 					wsUrl = eElement.getElementsByTagName("Url").item(0).getTextContent();
 				}
 			}
-			getLogger().info("WS hash value: " + hashValue);
-			getLogger().info("WS IP: " + wsIpAddress);
-			getLogger().info("WS URL: " + wsUrl);
+			getLogger().info("Values received from web service: HashValue = " + hashValue + ", IP = " + wsIpAddress + ", URL = " + wsUrl);
 		} catch (MalformedURLException e) {
 			getLogger().info("Malformed URL: " + e.getMessage());
 		} catch (ParserConfigurationException e) {
@@ -79,15 +81,15 @@ public class ModuleTokenValidate extends ModuleBase implements IMediaStreamNameA
 		// Validate token
 		// Get file and/or filename from Wowza cache or Mensch store 
 		String fileName = null;
-		getLogger().info(Url + " equals " + wsUrl + ": " + Url.equals(wsUrl));
-		getLogger().info(IpAddress + " equals " + wsIpAddress + ": " + IpAddress.equals(wsIpAddress));
-		if (Url.equals(wsUrl) && IpAddress.equals(wsIpAddress)) {
+		getLogger().info("Validate URL: Wowza URL = " + url + " equals web service URL =  " + wsUrl + ": " + url.equalsIgnoreCase(wsUrl));
+		getLogger().info("Validate IP: Wowza IP = " + ipAddress + " equals web service IP = " + wsIpAddress + ": " + ipAddress.equalsIgnoreCase(wsIpAddress));
+		if (url.equalsIgnoreCase(wsUrl) && ipAddress.equalsIgnoreCase(wsIpAddress)) {
 			File file = new File("C:/Program Files (x86)/Wowza Media Systems/Wowza Streaming Engine 4.2.0/content/" + hashValue + ".mp4");
 			BufferedInputStream in = null;
 			FileOutputStream fos = null;
 			if (file.exists()) {
 				fileName = "mp4:" + file.getName();
-				getLogger().info("Existing file name: " + fileName);
+				getLogger().info("Cache file name: " + fileName);
 			}
 			else {
 				try {
@@ -123,7 +125,6 @@ public class ModuleTokenValidate extends ModuleBase implements IMediaStreamNameA
 					}
 				}
 			}
-			
 		}
 		getLogger().info("Returned file name: " + fileName);
 		return fileName;
@@ -137,42 +138,15 @@ public class ModuleTokenValidate extends ModuleBase implements IMediaStreamNameA
 
 	public void onConnect(IClient client, RequestFunction function, AMFDataList params) {
 		getLogger().info("onConnect: " + client.getClientId());
-		
-		/*
-		tokenValidate = new TokenValidate(client);		
-		
-		if (!tokenValidate.validate()) {
-			getLogger().info("Client ID " + client.getClientId() + " not valid.");
-			client.rejectConnection("Invalid token.");
-			client.shutdownClient();
-		}
-		else {
-			// Get video from Mensch store via token/hash and send to RTMP player
-			client.acceptConnection("Valid token.");
-			IApplicationInstance appInstance = client.getAppInstance();
-			appInstance.setStreamNameAliasProvider(this);
-		}
-		*/
 	}
 	
 	public void onHTTPSessionCreate(IHTTPStreamerSession httpSession) {
 		getLogger().info("onHTTPSessionCreate: " + httpSession.getSessionId());
-		/*
-		tokenValidate = new TokenValidate(httpSession);
-		if (!tokenValidate.validate()) {
-			httpSession.rejectSession();
-			httpSession.shutdown();
-		}
-		*/
 	}
 	
 	@Override
 	public String resolvePlayAlias(IApplicationInstance appInstance, String name, IClient client) {
-		getLogger().info("Token: " + client.getQueryStr());
-		getLogger().info("Url: " + client.getPageUrl());
-		getLogger().info("Ip: " + client.getIp());
-		getLogger().info("Name: " + name);
-		getLogger().info("Validate token: " + validateToken(client.getQueryStr(), client.getPageUrl(), client.getIp()));
+		getLogger().info("resolvePlayAlias Wowza values: Token = " + client.getQueryStr() + ", URL = " + client.getPageUrl() + ", IP = " + client.getIp() + ", Name = " + name);
 		
 		try {
 			return validateToken(client.getQueryStr(), client.getPageUrl(), client.getIp());
